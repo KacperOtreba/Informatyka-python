d34bb644cbaa10e7896273e2ba4dbae3fa119ae7
KacperOtreba <kacperotreba1@gmail.com>
2024-05-15 14:44:45 +0200

    fix

--- Przyklady/CalkiSPR.py
+++ Przyklady/CalkiSPR.py
@@ -16,6 +16,9 @@ def calka(a,b, n, fun):
 # 1 worek nawozu = 5kg
 # 1 worek nawozu = 25m2 czyli 0,025km2
 # 1km = 1000m czyli 1m = 0,001km czyli 25m = 0,025km
+# POPRAWKA
+# 1km2 to 1000000m2
+# czyli musi być 0,000025m2
 
 
 Pole_dolnej_dzialki = calka(0,2, 17, f)
@@ -26,7 +29,7 @@ Pole_gornej_dzialki = Pole_calej_dzialki - Pole_dzialki_podRzeka
 
 Pole_dzialki_bezRzeka = Pole_dolnej_dzialki + Pole_gornej_dzialki
 
-Ilosc_workow = Pole_dzialki_bezRzeka / 0.025
+Ilosc_workow = Pole_dzialki_bezRzeka / 0.025 # Powinno być przez / 0.000025
 
 print("Powierzchnia dolnej czesci dzialki to ",Pole_dolnej_dzialki, "km^2")
 print("Powierzchnia górnej działki to ",Pole_gornej_dzialki, "km^2")
